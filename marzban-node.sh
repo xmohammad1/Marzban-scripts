6e88e537d0f77aea8d5ee1db61eb27a3743be71f
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2025-08-11 03:24:28 +0330

    Update marzban-node.sh

--- marzban-node.sh
+++ marzban-node.sh
@@ -306,6 +306,10 @@ services:
     image: xmohammad1/marzban-node:latest
     restart: always
     network_mode: host
+    ulimits:
+      nofile:
+        soft: 1048576
+        hard: 1048576
     environment:
       SSL_CLIENT_CERT_FILE: "/var/lib/marzban-node/cert.pem"
       SERVICE_PORT: "$SERVICE_PORT"
