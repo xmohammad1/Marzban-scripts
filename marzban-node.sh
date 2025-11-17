3d2c75da0765fe67981f6411249d9aca482657a1
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2025-11-17 05:28:52 +0330

    Remove ulimits from marzban-node.sh
    Removed ulimits configuration from the container settings.

--- marzban-node.sh
+++ marzban-node.sh
@@ -306,13 +306,6 @@ services:
     image: xmohammad1/marzban-node:latest
     restart: always
     network_mode: host
-    ulimits:
-      nofile:
-        soft: 1048576
-        hard: 1048576
-      nproc:
-        soft: 1048576
-        hard: 1048576
     environment:
       SSL_CLIENT_CERT_FILE: "/var/lib/marzban-node/cert.pem"
       SERVICE_PORT: "$SERVICE_PORT"
